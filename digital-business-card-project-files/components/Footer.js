8bb9904d6e59cfe573648896d5f016a0ff330422
Mustafa Noor <110131514+MustafaN435@users.noreply.github.com>
2023-09-20 16:51:02 +0500

    Add files via upload

---
+++ digital-business-card-project-files/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Footer(){
+    return(
+        <section className = "footer">
+            <img src="../images/Twitter-Icon.svg"/>
+            <img src="../images/Facebook-Icon.svg"/>
+            <img src="../images/Instagram-Icon.svg"/>
+            <img src="../images/Linkedin-Icon.svg"/>
+            <img src="../images/GitHub-Icon.svg"/>
+        </section>
+    )
+}
